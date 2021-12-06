df95219363ec8ac1931e88259fcc2e3b427f2c8b
dani54196 <dani54196@gmail.com>
2021-12-06 14:28:28 -0300

    drawing rectangles around the face

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,1 +1,27 @@
+import  cv2
+
+# Lodad some pre-trained data
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Chose and image
+img = cv2.imread('face1.jpeg')
+
+# Must convert to grayscale 
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces coordinates
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+# Draw rectangles around the face
+for (x, y, w, h) in face_coordinates:
+  cv2.rectangle(img, (x,y), (x+w, y+h),(0,255,0),2)
+
+print(face_coordinates)
+
+#
+cv2.imshow('Image', img)
+
+#
+cv2.waitKey()
+ 
 print ("Code Completed")
