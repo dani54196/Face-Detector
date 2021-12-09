a2714321382f4fbeaf52e0e3a91737d93e0821eb
dani54196 <dani54196@gmail.com>
2021-12-09 10:23:42 -0300

    draw rectangles

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,4 +1,5 @@
 import  cv2
+import random
 
 # Lodad some pre-trained data
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -7,23 +8,30 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread('face1.jpeg')
 # Campute video from webcamera
 webcam = cv2.VideoCapture(0)
+# interate forever over frames
+while True:
+  # Read the current frame
+  successful_frame_read, frame = webcam.read()
+  # must covert to grayscaled
+  grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# Must convert to grayscale 
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  # Detect faces coordinates
+  face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+  # Draw rectangles around the face
+  for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(frame, (x,y), (x+w, y+h),(random.randrange(256),random.randrange(256),random.randrange(256)),2)
+  print(face_coordinates)
 
-# Detect faces coordinates
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+  #
+  cv2.imshow('Image', frame)
+  cv2.waitKey(1)
 
-# Draw rectangles around the face
-for (x, y, w, h) in face_coordinates:
-  cv2.rectangle(img, (x,y), (x+w, y+h),(0,255,0),2)
 
-print(face_coordinates)
 
-#
-cv2.imshow('Image', img)
 
-#
-cv2.waitKey()
- 
+"""
+
+
+
+ """
 print ("Code Completed")
